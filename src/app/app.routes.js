b32d01de70b25325d0e1c9333efc11f12168999b
Jeremy Sik <jeremy.sik@me.com>
2017-04-04 22:00:58 +1000

    Removing js and js.map files, updating karma config

--- src/app/app.routes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.rootRouterConfig = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
-];
-//# sourceMappingURL=app.routes.js.map
--- src/app/tests/TrafficLightModel.spec.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=TrafficLightModel.spec.js.map
